8a7a6e2891e3efc91cd1359cf006a1732fd4ee13
Ryan Hemlock <ryan@ryanhemlock.com>
2024-02-09 14:28:29 -0500

    Refactored components

--- client/src/components/ProjectContainer.jsx
+++ client/src/components/ProjectContainer.jsx
@@ -1,21 +1,12 @@
-import CardPreview from './CardPreview'
+import List from './List'
 
 function ProjectContainer() {
     return (
         <div className="grid grid-flow-col auto-cols-max gap-x-3">
-            <div className="text-white black-bg px-3 w-72">
-                <h3>List Name</h3>
-                <CardPreview />
-                <CardPreview />
-                <CardPreview />
-                <CardPreview />
-                <CardPreview />
-            </div>
-            <div className="text-white black-bg px-3 w-72">
-                <h3>List Name</h3>
-            </div>
-            <div className="text-white black-bg px-3 w-72">
-                <h3>List Name</h3>
+            <List />
+            <List />
+            <div>
+                <button className='new-list-bttn'>Create a new list...</button>
             </div>
         </div>
     )
