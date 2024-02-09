b9c7e51c8b24303845db9dabaa98f91010cf8891
Mykhailo <sharkon90@gmail.com>
2024-02-09 12:48:56 -0500

    adding models for Card, List, Project

---
+++ server/models/Card.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const cardSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 30,
+        trim: true,
+    },
+    listId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    description: {
+        type: Text,
+
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    },
+    comments: [
+        {
+            commentText: {
+                type: String,
+                required: true,
+                minlength: 1,
+                maxlength: 280,
+            },
+            commentAuthor: {
+                type: String,
+                required: true,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now,
+                get: (timestamp) => dateFormat(timestamp),
+            },
+        },
+    ]
+});
+
+const Card = model('Card', cardSchema);
+
+module.exports = Card;
