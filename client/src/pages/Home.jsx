0b38944e94bb5633af0c1923e40c602c55da5c41
Mykhailo <sharkon90@gmail.com>
2024-02-12 11:14:59 -0500

    working on data

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,34 +1,36 @@
 // import { useState } from "react";
-// import { useQuery } from '@apollo/client';
+import {useQuery} from "@apollo/client";
 import ProjectContainer from "../components/ProjectContainer";
-import CardModal from "../components/CardModal";
+// import CardModal from "../components/CardModal";
 
-// import { QUERY_PROJECTS } from '../utils/queries'
+import {QUERY_ONE_PROJECT} from "../utils/queries";
 
-// const { data } = useQuery(QUERY_PROJECTS);
 // const projects = data?.projects || [];
+// const {data} = useQuery(QUERY_SINGLE_PROJECT);
 
 // console.log(projects)
 
 function Home() {
-    // const [modalOpen, setModalOpen] = useState(false)
-
-    return (
-        <div className="gradient-bg px-5 h-screen">
-            {/* {loading ? (
+  // const [modalOpen, setModalOpen] = useState(false)
+  const {data} = useQuery(QUERY_ONE_PROJECT, {
+    variables: {projectId: "65ca3d0fe0a69bbcb868de5c"},
+  });
+  console.log(data);
+  return (
+    <div className="gradient-bg px-5 h-screen">
+      {/* {loading ? (
                 <div>Loading...</div>
             ) : (
                 <ProjectContainer />
             )} */}
-            <ProjectContainer />
+      <ProjectContainer />
 
-            {/* {modalOpen && (
+      {/* {modalOpen && (
                 <CardModal />
             )} */}
-            <CardModal />
-
-        </div>
-    )
+      {/* <CardModal /> */}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
