5706eebb72aa4193956527cdc832212f55ffd76e
MMK <staph103@gmail.com>
2024-02-14 13:29:53 -0500

    quieres

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -33,6 +33,9 @@ query getProjectByID($projectId: ID!) {
       _id
       username
       email
+      gitHub
+      linkedIn
+      img
     }
     lists {
       _id
@@ -50,8 +53,14 @@ query getProjectByID($projectId: ID!) {
           commentAuthor
           createdAt
         }
+        toDoes {
+          _id
+          text
+          isCompleted
+        }
       }
     }
+    createdBy
   }
 }
 `;
@@ -78,3 +87,51 @@ query GetCard($cardId: ID!) {
   }
 }
 `;
+
+export const GET_USER = gql`
+query GetUser($userId: ID!) {
+  user(userId: $userId) {
+    _id
+    username
+    email
+    gitHub
+    img
+    projects {
+      _id
+      title
+      createdAt
+      createdBy
+      users {
+        _id
+        username
+        email
+        img
+      }
+      lists {
+        _id
+        title
+        projectId
+        cards {
+          _id
+          title
+          listId
+          description
+          createdAt
+          comments {
+            _id
+            commentText
+            commentAuthor
+            createdAt
+          }
+          toDoes {
+            _id
+            text
+            isCompleted
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
